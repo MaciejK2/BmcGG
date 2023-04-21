4c476f1f062f5d165af9695709c0def26bc6b812
Zircta <126593996+Zircta@users.noreply.github.com>
2023-04-21 13:36:34 -0400

    Up version + remove unused

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,19 +4,16 @@ plugins {
 }
 
 group = "me.zircta"
-version = "1.0"
+version = "1.0.5"
 
 minecraft.version("1.8.9")
 
 repositories {
     maven("https://jitpack.io")
-    maven("https://repo.spongepowered.org/maven/")
 }
 
 dependencies {
     compileOnly("com.github.weave-mc:weave-loader:6a9e6a3245")
-
-    compileOnly("org.spongepowered:mixin:0.8.5")
 }
 
 tasks.compileJava {
