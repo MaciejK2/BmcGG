690f6f2753c505b93d05e5b23381347ff2ad7f67
Maciej <75207983+MaciejK2@users.noreply.github.com>
2023-06-09 15:06:09 +0200

    Update BlocksGG.java

--- src/main/java/me/zircta/blocksgg/BlocksGG.java
+++ src/main/java/me/zircta/blocksgg/BlocksGG.java
@@ -1,12 +1,12 @@
 package me.zircta.blocksgg;
 
-import club.maxstats.weave.loader.api.ModInitializer;
-import club.maxstats.weave.loader.api.event.EventBus;
+import net.weavemc.loader.api.ModInitializer;
+import net.weavemc.loader.api.event.EventBus;
 import me.zircta.blocksgg.events.ChatEvent;
 
 public class BlocksGG implements ModInitializer {
     @Override
-    public void init() {
+    public void preInit() {
         System.out.println("BlocksGG | Initializing");
 
         EventBus.subscribe(new ChatEvent());
